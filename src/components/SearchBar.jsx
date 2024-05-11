2d35b1359d7c535a46f192bab30ec0aff55d22d6
amora7741 <mora7741@gmail.com>
2024-05-10 17:01:31 -0700

    Add search button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -15,7 +15,7 @@ const SearchBar = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label className='input input-bordered flex items-center gap-2 bg-transparent w-full'>
+      <label className='input input-bordered flex items-center gap-2 bg-transparent w-full p-2'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           className='w-5 h-5 opacity-70'
@@ -31,18 +31,20 @@ const SearchBar = () => {
           onChange={(e) => setLocation(e.target.value)}
           placeholder='Enter your location'
         />
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 16 16'
-          fill='currentColor'
-          className='w-4 h-4 opacity-70'
-        >
-          <path
-            fillRule='evenodd'
-            d='M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z'
-            clipRule='evenodd'
-          />
-        </svg>
+        <button className='btn btn-ghost p-2 min-h-0 h-fit' type='submit'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 16 16'
+            fill='currentColor'
+            className='w-4 h-4 opacity-70'
+          >
+            <path
+              fillRule='evenodd'
+              d='M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z'
+              clipRule='evenodd'
+            />
+          </svg>
+        </button>
       </label>
     </form>
   );
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,9 +1,22 @@
-import SearchBar from './SearchBar';
+'use client';
 
-const WeatherCard = ({ weatherData }) => {
+import SearchBar from './SearchBar';
+import { useContext } from 'react';
+import { DataContext } from '@/app/DataContext';
+
+const WeatherCard = () => {
+  const { weatherData } = useContext(DataContext);
+
   return (
-    <div className='w-[400px] h-[100px] flex flex-col items-center justify-center text-white bg-white/[.1] backdrop-blur-md border-2 border-white/20 rounded-2xl p-5'>
+    <div className='w-[400px] flex flex-col gap-8 items-center justify-center text-white text-center bg-white/[.1] backdrop-blur border-2 border-white/20 rounded-2xl p-5 transition-[height]'>
       <SearchBar />
+      {weatherData && (
+        <>
+          <h1 className='text-3xl font-semibold text-pretty'>
+            {weatherData.location.name}, {weatherData.location.country}
+          </h1>
+        </>
+      )}
     </div>
   );
 };
