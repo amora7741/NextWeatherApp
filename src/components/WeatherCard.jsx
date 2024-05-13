f8951efba0e52f30cab678a2f5a8554e3df1b076
amora7741 <mora7741@gmail.com>
2024-05-13 14:12:19 -0700

    Display weather icon, temp and condition

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,12 +1,16 @@
 'use client';
 
 import SearchBar from './SearchBar';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { DataContext } from '@/app/DataContext';
 import Image from 'next/image';
 
 const WeatherCard = () => {
   const { weatherData } = useContext(DataContext);
+
+  useEffect(() => {
+    console.log(weatherData);
+  }, [weatherData]);
 
   return (
     <div className='w-[400px] flex flex-col gap-8 items-center justify-center text-white text-center bg-white/[.1] backdrop-blur border-2 border-white/20 rounded-2xl p-5 transition-[height]'>
@@ -25,9 +29,28 @@ const WeatherCard = () => {
         <>
           {weatherData && (
             <>
-              <h1 className='text-3xl font-semibold'>
+              <h1 className='text-2xl font-semibold max-w-[80%]'>
                 {weatherData.location.name}, {weatherData.location.country}
               </h1>
+              <Image
+                src={`https:${weatherData.current.condition.icon}`}
+                width={100}
+                height={100}
+                alt='Weather icon'
+              />
+              <div className='flex flex-col items-center justify-center'>
+                <div className='relative'>
+                  <h2 className='text-6xl font-bold'>
+                    {Math.round(weatherData.current.feelslike_f)}
+                  </h2>
+                  <h2 className='text-2xl font-semibold absolute -top-2 -right-6'>
+                    °F
+                  </h2>
+                </div>
+                <p className='capitalize font-medium'>
+                  {weatherData.current.condition.text}
+                </p>
+              </div>
             </>
           )}
         </>
