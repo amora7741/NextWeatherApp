47fa6c490549c2c405c5b05237324ed61c8da4b2
amora7741 <mora7741@gmail.com>
2024-05-13 13:32:33 -0700

    Add not found image

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -3,6 +3,7 @@
 import SearchBar from './SearchBar';
 import { useContext } from 'react';
 import { DataContext } from '@/app/DataContext';
+import Image from 'next/image';
 
 const WeatherCard = () => {
   const { weatherData } = useContext(DataContext);
@@ -11,7 +12,15 @@ const WeatherCard = () => {
     <div className='w-[400px] flex flex-col gap-8 items-center justify-center text-white text-center bg-white/[.1] backdrop-blur border-2 border-white/20 rounded-2xl p-5 transition-[height]'>
       <SearchBar />
       {weatherData === 'DNE' ? (
-        <h1 className='text-3xl font-semibold'>Location not found</h1>
+        <>
+          <Image
+            src='/404.png'
+            width='300'
+            height='200'
+            alt='Not found image'
+          />
+          <h1 className='text-2xl font-semibold mb-8'>Location not found!</h1>
+        </>
       ) : (
         <>
           {weatherData && (
