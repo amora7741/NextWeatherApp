9fe70dd3ec228efc4a483ce63eb73639f317a9f4
amora7741 <mora7741@gmail.com>
2024-05-15 17:20:25 -0700

    Add toggle for temp units

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -40,21 +40,33 @@ const rightSlideVariants = {
 const WeatherCard = () => {
   const { weatherData } = useContext(DataContext);
   const [height, setHeight] = useState(100);
+  const [unit, setUnit] = useState('F');
 
   useEffect(() => {
-    console.log(weatherData);
     setHeight(weatherData ? 'auto' : 100);
   }, [weatherData]);
+
+  const toggleUnit = () => {
+    setUnit(unit === 'F' ? 'C' : 'F');
+  };
 
   return (
     <AnimateHeight duration={300} height={height}>
       <div className='flex flex-col gap-6 items-center justify-center text-white text-center bg-black/[.1] backdrop-blur-xl border-2 border-white/20 rounded-2xl p-5'>
-        <SearchBar />
+        <div className='flex gap-4 items-center'>
+          <SearchBar />
+        </div>
         <AnimatePresence>
           {weatherData === 'DNE' ? (
             <NotFound />
           ) : (
-            weatherData && <WeatherDetails weatherData={weatherData} />
+            weatherData && (
+              <WeatherDetails
+                weatherData={weatherData}
+                unit={unit}
+                toggleUnit={toggleUnit}
+              />
+            )
           )}
         </AnimatePresence>
       </div>
@@ -84,11 +96,14 @@ const NotFound = () => (
   </>
 );
 
-const WeatherDetails = ({ weatherData }) => {
+const WeatherDetails = ({ weatherData, unit, toggleUnit }) => {
   const {
     location: { name, region, country },
-    current: { condition, feelslike_f, humidity, wind_mph },
+    current: { condition, feelslike_f, feelslike_c, humidity, wind_mph },
   } = weatherData;
+
+  const temperature =
+    unit === 'F' ? Math.round(feelslike_f) : Math.round(feelslike_c);
 
   return (
     <>
@@ -122,17 +137,27 @@ const WeatherDetails = ({ weatherData }) => {
         initial='hidden'
         animate='visible'
         variants={topSlideVariants}
-        key={feelslike_f}
+        key={temperature}
       >
         <div className='relative -ml-5'>
-          <h2 className='text-6xl font-bold'>{Math.round(feelslike_f)}</h2>
+          <h2 className='text-6xl font-bold'>{temperature}</h2>
           <h2 className='text-2xl font-semibold absolute -top-2 -right-7'>
-            °F
+            °{unit}
           </h2>
         </div>
         <p className='capitalize font-medium text-xl'>{condition.text}</p>
       </motion.div>
-      <div className='flex justify-around w-full'>
+      <div className='flex items-center gap-2'>
+        <p>F</p>
+        <input
+          type='checkbox'
+          className='toggle toggle-primary'
+          checked={unit === 'C'}
+          onChange={toggleUnit}
+        />
+        <p>C</p>
+      </div>
+      <div className='flex justify-around w-full mt-4 mb-4'>
         <motion.div
           custom={3}
           initial='hidden'
